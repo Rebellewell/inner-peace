a940040978d3d1adeb4f444a87f0a48ce6c549f9
Rebellewell <56415164+Rebellewell@users.noreply.github.com>
2020-03-05 19:41:18 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import About from './components/About';
+import CommonQuestions from './components/CommonQuestions';
+import ContactUs from './components/ContactUs';
+import Nav from './components/Nav';
+import PatientForms from './components/PatientForms';
+import Photos from './components/Photos';
+import PrivacyPolicy from './components/PrivacyPolicy';
+import Rates from './components/Rates';
+import Services from './components/Rates';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/services" component={Services} />
+        <Route path="/patientforms" component={PatientForms} />
+        <Route path="/services" component={Services} />
+        <Route path="/commonquestions" component={CommonQuestions} />
+        <Route path="/rates" component={Rates} />
+        <Route path="/privacypolicy" component={PrivacyPolicy} />
+        <Route path="/contactus" component={ContactUs} />
+        <Route path="/photos" component={Photos} />
+      </div>
+    </Router>
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+);
 
 export default App;
